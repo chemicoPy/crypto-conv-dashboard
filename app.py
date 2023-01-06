b5cfc405dfb464ca4ee02c88b45f82fd4b3ee2cc
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-06 15:22:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,8 +99,8 @@ bybit = ccxt.bybit()
 lim = 1000
 
 instrument = st.sidebar.selectbox(
-        '', ["Select Forex Pair of interest", "MATIC/USDT" , "XAU/USDT","BTC/USDT","ETH/USDT","DOGE/USDT", "GBP/USDT", "GBP/JPY", 
-             "USD/JPY", "EUR/USDT", "NZD/USDT"], index=0)
+        '', ["Select Forex Pair of interest", "MATIC/USDT" , "XAU/USDT","BTC/USDT","ETH/USDT","DOGE/USDT", "GBP/USDT", 
+             "EUR/USDT", "NZD/USDT"], index=0)
   
 Tframe = st.sidebar.selectbox(
         '', ["Interval of interest", "1m","5m","15m","30m","1h","2h","1d","1w", "month"], index=0)
