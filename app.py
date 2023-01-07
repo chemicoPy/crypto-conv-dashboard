e5f8b79991c1f17ab4c1c740cfa2001e9a19e697
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-07 17:24:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ instrument = st.sidebar.selectbox(
         '', ["Select Forex Pair of interest", "MATIC/USDT" , "XAU/USDT","BTC/USDT","ETH/USDT","DOGE/USDT", "GBP/USDT", 
              "EUR/USDT", "NZD/USDT"], index=0)
 Tframe = st.sidebar.selectbox(
-        '', ["Interval of interest", "1m","5m","15m","30m","1h","2h","1d","1w", "month"], index=0)
+        '', ["Interval of interest", "1m","5m","15m","30m","1h","2h","4h","5h","1d","1w", "month"], index=0)
  
     
 st.write("\n")  # add spacing    
