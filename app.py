92134de228361153021b1062d20a1e192d88c505
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-08 13:12:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ to_conv = st.selectbox(
              "EUR", "NZD"),)
 
 from google_currency import convert 
-response = convert("usd", "inr", price)
+response = convert(from_conv.lower(), to_conv.lower(), price)
 st.write(response)
 
 #result = c.convert(from_conv, to_conv, price)
