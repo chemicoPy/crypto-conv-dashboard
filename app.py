49f76120c48701cc4c3e3722f7e9d040b5d2aa5a
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-09 21:11:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ to_conv = st.selectbox(
                                                   "CHF","ISK","NOK","TRY","AUD","BRL","CAD","CNY","HKD","IDR","ILS",
                                                   "INR","KRW","MXN","MYR","PHP","SGD", "THB", "ZAR"),)
     
-st.write(simpleConverter.convert(price, 'USD', 'BTC'))
+st.write(simpleConverter.convert(price, str(from_conv), 'BTC'))
 #st.write("Converted price= ", (c.get_symbol(to_conv), simpleConverter.convert(price, from_conv, to_conv)) 
     
 
