2862cde0eacb0c5e5bf5fa9c2cf09a0160ed1416
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-16 02:56:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,50 +123,30 @@ price = st.number_input("Enter price to convert")
 st.write("Converted price = ", simpleConverter.convert(price, str(from_conv), str(to_conv)))
 #st.write("Converted price= ", (c.get_symbol(to_conv), simpleConverter.convert(price, from_conv, to_conv)) 
     
-import base64
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(png_file):
-    with open(png_file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-def build_markup_for_logo(
-    png_file,
-    background_position="50% 10%",
-    margin_top="10%",
-    image_width="60%",
-    image_height="",
-):
-    binary_string = get_base64_of_bin_file(png_file)
-    return """
-            <style>
-                [data-testid="stSidebarNav"] {
-                    background-image: url("data:image/png;base64,%s");
-                    background-repeat: no-repeat;
-                    background-position: %s;
-                    margin-top: %s;
-                    background-size: %s %s;
-                }
-            </style>
-            """ % (
-        binary_string,
-        background_position,
-        margin_top,
-        image_width,
-        image_height,
-    )
-
-
-def add_logo(png_file):
-    logo_markup = build_markup_for_logo(png_file)
+def add_logo():
     st.markdown(
-        logo_markup,
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://github.com/chemicoPy/crypto-conv-dashboard/blob/main/img/page_icon.png);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
         unsafe_allow_html=True,
     )
 
-add_logo("img/page_icon.png")
+add_logo()
 
   
     # ------ layout setting---------------------------
