0e8968c9d3f0966e9ada7d28b188b16c2cd09e60
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-16 02:47:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,15 +63,23 @@ st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-st.markdown(
-   f”””
-   <style>
-   p {
-   background-image: url(‘img/page_icon.png’);
-   }
-   </style>
-   ”””,
-   unsafe_allow_html=True)
+
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('img/page_icon.png')
 
 st.title('Crypto Converter to Local Currency')
 st.subheader("Navigate to side bar to see more options as well as full project info")
