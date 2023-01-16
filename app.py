185959b8235577009855f84920f3e9c14e9c17cc
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-16 03:08:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,9 @@ instrument = st.sidebar.selectbox(
         '', ["Select Forex Pair of interest", "MATIC/USDT" , "XAU/USDT","BTC/USDT","ETH/USDT","DOGE/USDT"], index=0)
 Tframe = st.sidebar.selectbox(
         '', ["Interval of interest", "1m","5m","15m","30m","1h","2h","4h","1d","1w","month"], index=0)
-    
+
+if st.button("Show Viz!"):
+  st.write("Success!")
     
 st.sidebar.markdown(
 
