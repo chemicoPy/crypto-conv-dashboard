1ffe44454cc29a400d206f79641b5b36853f35aa
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-16 11:02:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -156,8 +156,9 @@ to_conv = st.sidebar.selectbox(
     
 price = st.sidebar.number_input("Enter price to convert")
 
-st.sidebar.write("Converted price = ", simpleConverter.convert(price, str(from_conv), str(to_conv[:3])))
-#st.write("Converted price= ", (c.get_symbol(to_conv), simpleConverter.convert(price, from_conv, to_conv)) 
+if st.sidebar.button("Convert"):
+  st.sidebar.write("Converted Price = ", simpleConverter.convert(price, str(from_conv), str(to_conv[:3])))
+  #st.write("Converted price= ", (c.get_symbol(to_conv), simpleConverter.convert(price, from_conv, to_conv)) 
 
     # ---------------forex pair selection------------------
   
