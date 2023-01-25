2f1d59b4341bddaa4a9aeaa45f2466b29a5cc081
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2023-01-26 00:17:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,9 @@ import statsmodels.regression.linear_model as rg
 # Desiging & implementing changes to the standard streamlit UI/UX
 st.set_page_config(page_icon="img/page_icon.png", layout='wide', initial_sidebar_state='expanded')    #Logo
 
+st.title('Crypto Converter to Local Currency')
+st.subheader("Navigate to side bar to see full project info as well as options to choose from, to get started!")
+
 from forex_python.converter import CurrencyRates
 from forex_python.converter import CurrencyCodes
 
